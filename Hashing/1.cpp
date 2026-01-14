c5360158940793a04d3af648a6edb163f6ebe48b
Ritik7307 <vijayshankarprajapati29@gmail.com>
2026-01-14 22:52:45 +0530

    hashing

--- Hashing/1.cpp
+++ Hashing/1.cpp
@@ -7,10 +7,19 @@ int main(){
      for(int i=0;i<n;i++){
         cin>>arr[i];
      }
+   //   pre computation
+
+   int hash[13]={0};
+   for(int i=0;i<n;i++){
+      hash[arr[i]]++;
+   }
      int q;
      cout<<"Enter queris: ";
      cin>>q;
      while(q--){
-
+      int number;
+      cin>>number;
+      // fetch
+      cout<<hash[number]<<endl;
      }
 }
---
+++ Hashing/2.cpp
@@ -0,0 +1,25 @@
+// character hashinng
+
+// s = abcdabefgc
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+
+    // pre computation
+    int hash[26]={0};
+    for(int i=0;i<s.size();i++){
+        hash[s[i]-'a']++;
+    }
+
+    int q;
+    cout<<"ener queries:";
+    cin>>q;
+    while(q--){
+        char c;
+        cin>>c;
+        // fetch
+        cout<<hash[c-'a']<<endl;
+    }
+}
